b903df8abfe8aa096ee87e44badbe8a17341409c
renato <felicio95.jr@gmail.com>
2025-06-20 19:55:14 -0300

    chore: initial commit

---
+++ FeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedApp
+//
+//  Created by Renato F. dos Santos Jr on 20/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
